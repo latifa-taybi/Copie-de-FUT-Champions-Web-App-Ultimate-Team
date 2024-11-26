1285f2a9cc087592fc5995d22543eaa8d9a175df
latifa <taybilatifa46@gmail.com>
2024-11-26 16:55:09 +0100

    HTML et CSS

---
+++ main.js
@@ -0,0 +1,26 @@
+const add=document.getElementById("add");
+
+
+add.addEventListener('click',()=>{
+    const name=document.getElementById("name").value;
+    const photo=document.getElementById("photo").value;
+    const position=document.getElementById("position");
+    const Nationalité=document.getElementById("Nationalité").value;
+    const flag=document.getElementById("flag").value;
+    const logo=document.getElementById("logo").value;
+
+    const pace=document.getElementById("pace").value;
+    const shooting=document.getElementById("shooting").value;
+    const passing=document.getElementById("passing").value;
+    const dribbling=document.getElementById("dribbling").value;
+    const defending=document.getElementById("defending").value;
+    const physical=document.getElementById("physical").value;
+
+    let données=[];
+    
+
+})
+    
+    
+
+
